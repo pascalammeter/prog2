3d8f545912db6b1c2b7b89e89d062d96e0595b9c
Pascal Ammeter <pascal.ammeter@infosec.ch>
2021-05-05 21:09:47 +0200

    bootstrap & json exercises/workout added

--- Trainingsplan/libs/eingabedaten_speichern.py
+++ Trainingsplan/libs/eingabedaten_speichern.py
@@ -1,46 +1,35 @@
 from datetime import datetime
 import json
-# from libs import data
 
 
-def speichern(datei, key, value, groesse, gewicht, alter):
+def speichern(entry):
     try:
-        with open(datei) as open_file:
+        with open("data/data.json") as open_file:
             datei_inhalt = json.load(open_file)
     except FileNotFoundError:
         datei_inhalt = {}
 
-    datei_inhalt[str(key)] = value
+    # datei_inhalt[str(key)] = value
+    datei_inhalt.update(entry)
 
-    print(datei_inhalt)
-
-    with open(datei, "w") as open_file:
+    with open("data/data.json", "w") as open_file:
         json.dump(datei_inhalt, open_file)
-
-
-def eingabedaten(vorname, nachname, geschlecht, groesse, gewicht, alter):
-    json_daten = "data.json"
-    alle_personen = json_daten.get('personen', {})
-    person = {"vorname": vorname, "nachname": nachname, "geschlecht": geschlecht, "groesse": groesse, "gewicht": gewicht, "alter": alter}
-    alle_personen['person'][gewicht] = person
-    json_daten["personen"] = alle_personen
-    speichern(vorname, nachname, geschlecht, groesse, gewicht, alter)
-
-
-def aktivitaet_speichern(aktivitaet):
-    datei_name = "data.json"
-    zeitpunkt = datetime.now()
-    speichern(datei_name, zeitpunkt, aktivitaet)
-    return zeitpunkt, aktivitaet
-
-
-def aktivitaeten_laden():
-    datei_name = "aktivitaeten.json"
-
-    try:
-        with open(datei_name) as open_file:
-            datei_inhalt = json.load(open_file)
-    except FileNotFoundError:
-        datei_inhalt = {}
-
-    return datei_inhalt
+#
+#
+# def aktivitaet_speichern(aktivitaet):
+#     datei_name = "workout.json"
+#     zeitpunkt = datetime.now()
+#     speichern(datei_name, zeitpunkt, aktivitaet)
+#     return zeitpunkt, aktivitaet
+#
+#
+# def aktivitaeten_laden():
+#     datei_name = "aktivitaeten.json"
+#
+#     try:
+#         with open(datei_name) as open_file:
+#             datei_inhalt = json.load(open_file)
+#     except FileNotFoundError:
+#         datei_inhalt = {}
+#
+#     return datei_inhalt
--- Trainingsplan/main.py
+++ Trainingsplan/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
-from libs import eingabedaten_speichern
+
+from libs.eingabedaten_speichern import speichern
 
 
 app = Flask("Trainingsplan-Generator")
@@ -7,12 +8,13 @@ app = Flask("Trainingsplan-Generator")
 
 @app.route("/")
 def startseite():
+    name = False
     # Summary:
     # Zeigt die Startseite an, wenn die url/route '/' ist.
     # Auf der Startseite kann man wählen, ob ein Trainingsplan erstellt werden soll.
     # Returns:
     # template: Das HTML 'index.html' wird gerendert mit der Auswahlmöglichkeit
-    return render_template('index.html')
+    return render_template('index.html', name=name)
 
 
 @app.route("/trainingsplan", methods=['GET', 'POST'])
@@ -21,37 +23,42 @@ def trainingsplan():
     # Hier kann der Trainingsplan erstellt werden. Dieser wird anhand der Eingabedaten generiert.
     # Die url/route ist '/trainingsplan'.
     if request.method == 'POST':
-        print(request.form)
         vorname = request.form['vorname']
         nachname = request.form['nachname']
         geschlecht = request.form['geschlecht']
         groesse = request.form['groesse']
-        gewicht = request.form['gewicht']
         alter = request.form['alter']
+        erfahrung = request.form['erfahrung']
+        ziel = request.form['ziel']
+        frequenz = request.form['frequenz']
+        zeitplan = request.form['zeitplan']
+        entry = {f"{vorname}_{nachname}":[geschlecht, groesse, alter, erfahrung, ziel, frequenz, zeitplan]}
+        print(entry)
+        speichern(entry)
         # Daten in JSON File speichern, Funktion eingabedaten in eingabedaten_speichern.py
-        returned_data = eingabedaten_speichern.eingabedaten(vorname, nachname, geschlecht, groesse, gewicht, alter)
-        # Weiterleitug auf Übungen, primar key gewicht mitnehmen
-        return redirect(url_for('uebungen'))
+        # returned_data = eingabedaten_speichern.eingabedaten(vorname, nachname, geschlecht, groesse, gewicht, alter)
+        # Weiterleitug auf Übungen, primar key mitnehmen?
+        # return redirect(url_for('uebungen'))
         # Wenn nicht ausgefüllt, Startseite Laden
     return render_template("eingabedaten.html")
 
-@app.route("/speichern/<aktivitaet>")
-def speichern(aktivitaet):
-    zeitpunkt, aktivitaet = eingabedaten_speichern.aktivitaet_speichern(aktivitaet)
-
-    return "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
-
-
-@app.route("/liste")
-def auflisten():
-    aktivitaeten = eingabedaten_speichern.aktivitaeten_laden()
-
-    aktivitaeten_liste = ""
-    for key, value in aktivitaeten.items():
-        zeile = str(key) + ": " + value + "<br>"
-        aktivitaeten_liste += zeile
-
-    return aktivitaeten_liste
+# @app.route("/speichern/<aktivitaet>")
+# def speichern(aktivitaet):
+#     zeitpunkt, aktivitaet = eingabedaten_speichern.aktivitaet_speichern(aktivitaet)
+#
+#     return "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
+#
+#
+# @app.route("/liste")
+# def auflisten():
+#     aktivitaeten = eingabedaten_speichern.aktivitaeten_laden()
+#
+#     aktivitaeten_liste = ""
+#     for key, value in aktivitaeten.items():
+#         zeile = str(key) + ": " + value + "<br>"
+#         aktivitaeten_liste += zeile
+#
+#     return aktivitaeten_liste
 
 @app.route("/uebungen")
 def uebungen():
